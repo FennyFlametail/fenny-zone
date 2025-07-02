4a32a1194918ab7c99547b0d329609aff7e0ca88
Fenny Flametail <fennyflametail@icloud.com>
2025-07-02 18:27:21 -0500

    fix: update Nocturne bio

--- src/routes/characters/nocturne/+page.svelte
+++ src/routes/characters/nocturne/+page.svelte
@@ -9,98 +9,60 @@
 		<li>Height: 5 ft 10 in (excluding horns)</li>
 		<li>Pronouns: they/them</li>
 		<li>Orientation: pansexual (masculine preference)</li>
-		<li>Space AU:</li>
-		<ul>
-			<li>Age: 20</li>
-			<li>
-				Personality: serious, reserved, analytical, rarely smiles
-				<ul>
-					<li>good-hearted but abrasive, easily irritated</li>
-					<li>confident, but very self-critical</li>
-					<li>doesn't like asking for help</li>
-					<li>tries not to get attached to people (afraid of losing them)</li>
-				</ul>
-			</li>
-			<li>Occupation: renegade, freedom fighter</li>
-			<li>
-				Skills:
-				<ul>
-					<li>quick-thinking</li>
-					<li>acrobatic (skilled at climbing and parkour)</li>
-					<li>expert shooter (prefers pistols)</li>
-					<li>can hold their own hand-to-hand, but prefers to run</li>
-					<li>good night vision</li>
-					<li>high tolerance for cold</li>
-				</ul>
-			</li>
-			<li>Favorite color: purple</li>
-			<li>Favorite food: fresh produce (hard to find)</li>
-			<li>Favorite drink: real coffee (also hard to find)</li>
-			<li>
-				Clothing style: techwear, black and white with small accents of color
-				<ul>
-					<li>nothing baggy or loose hanging</li>
-					<li>not militaristic (no camo)</li>
-					<li>doesn't like showing bare skin</li>
-				</ul>
-			</li>
-			<li>
-				Typical outfit:
-				<ul>
-					<li>jacket gifted to them by their former crew (the only possession they care about)</li>
-					<li>optional: pink AR glasses on forehead</li>
-					<li>t-shirt or tank top</li>
-					<li>fingerless gloves</li>
-					<li>black pants and socks</li>
-					<li>black and white sneakers or boots</li>
-				</ul>
-			</li>
-			<li>
-				Notes:
-				<ul>
-					<li>left-handed</li>
-					<li>prosthetic right arm</li>
-					<li>dislikes heat, swimming, processed food</li>
-					<li>uses guns, but doesn't fetishize them</li>
-					<li>doesn’t enjoy killing, but will in self-defense or to protect the vulnerable</li>
-				</ul>
-			</li>
-		</ul>
-		<li>Modern AU:</li>
-		<ul>
-			<li>Age: 28</li>
-			<li>
-				Personality: calm, organized, direct but not harsh, stylish but not pretentious
-				<ul>
-					<li>smiles a lot more</li>
-					<li>still gets irritated easily, but handles it calmly</li>
-				</ul>
-			</li>
-			<li>Job: owns a small record store next to the cafe Aren works at</li>
-			<li>
-				Interests: music, analog technology, books, gardening
-				<ul>
-					<li>likes to sing, especially with Aren playing the guitar</li>
-				</ul>
-			</li>
-			<li>Favorite food: fresh spring rolls</li>
-			<li>Favorite drink: oat milk latte</li>
-			<li>Favorite ice cream flavor: a really good vanilla</li>
-			<li>Favorite music genres: indie rock, lo-fi, ambient</li>
-			<li>Clothing style: modern casual, basic but stylish (Uniqlo vibes)</li>
-			<li>
-				Typical outfit:
-				<ul>
-					<li>black jacket or gray denim jacket</li>
-					<ul>
-						<li>nonbinary flag pin</li>
-					</ul>
-					<li>solid color or band logo tee</li>
-					<li>black or blue denim jeans</li>
-				</ul>
-			</li>
-			<li>Vehicle: NIU RQi-GT</li>
-		</ul>
+		<li>Age: 20</li>
+		<li>
+			Personality: serious, reserved, analytical, rarely smiles
+			<ul>
+				<li>good-hearted but abrasive, easily irritated</li>
+				<li>confident, but very self-critical</li>
+				<li>doesn't like asking for help</li>
+				<li>tries not to get attached to people (afraid of losing them)</li>
+			</ul>
+		</li>
+		<li>Occupation: renegade/freedom fighter, on the run from a totalitarian corporate empire</li>
+		<li>
+			Skills:
+			<ul>
+				<li>quick-thinking</li>
+				<li>acrobatic (skilled at climbing and parkour)</li>
+				<li>expert shooter (prefers pistols)</li>
+				<li>can hold their own hand-to-hand, but prefers to run</li>
+				<li>good night vision</li>
+				<li>high tolerance for cold</li>
+			</ul>
+		</li>
+		<li>Favorite color: purple</li>
+		<li>Favorite food: fresh produce (hard to find)</li>
+		<li>Favorite drink: real coffee (also hard to find)</li>
+		<li>
+			Clothing style: techwear, black and white with small accents of color
+			<ul>
+				<li>nothing baggy or loose hanging</li>
+				<li>not militaristic (no camo)</li>
+				<li>doesn't like showing bare skin</li>
+			</ul>
+		</li>
+		<li>
+			Typical outfit:
+			<ul>
+				<li>jacket gifted to them by their former crew (the only possession they care about)</li>
+				<li>optional: pink AR glasses on forehead</li>
+				<li>t-shirt or tank top</li>
+				<li>fingerless gloves</li>
+				<li>black pants and socks</li>
+				<li>black and white sneakers or boots</li>
+			</ul>
+		</li>
+		<li>
+			Notes:
+			<ul>
+				<li>left-handed</li>
+				<li>prosthetic right arm</li>
+				<li>dislikes heat, swimming, processed food</li>
+				<li>uses guns, but doesn't fetishize them</li>
+				<li>doesn’t enjoy killing, but will in self-defense or to protect the vulnerable</li>
+			</ul>
+		</li>
 		<li>
 			Artwork by <a target="_blank" href="https://www.furaffinity.net/user/candymint/">candymint</a>
 		</li>
