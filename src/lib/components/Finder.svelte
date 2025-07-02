8577cfb82ae8811d737ad583a592defb33603fdf
Fenny Flametail <fennyflametail@icloud.com>
2025-07-01 19:23:10 -0500

    feat: add status bar to Finder

--- src/lib/components/Finder.svelte
+++ src/lib/components/Finder.svelte
@@ -5,23 +5,40 @@
 	const { children }: { children: Snippet } = $props();
 
 	const { onClickIconContainer } = setupFileIconContainer();
+
+	let icons = $state<HTMLDivElement>();
 </script>
 
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <div class="finder" onclick={onClickIconContainer}>
-	{@render children()}
+	<div class="finderStatusBar">{icons?.childElementCount ?? ''} items, ∞ GB available</div>
+	<div class="finderIcons" bind:this={icons}>
+		{@render children()}
+	</div>
 </div>
 
 <style>
 	.finder {
 		min-height: 100%;
+		background-color: white;
+	}
+
+	.finderStatusBar {
+		height: 19px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-bottom: 1px solid #8c8c8c;
+		font-size: 12px;
+	}
+
+	.finderIcons {
 		display: grid;
 		grid-template-columns: repeat(auto-fill, 128px);
 		justify-content: space-evenly;
 		align-content: start;
 		row-gap: 42px;
 		padding-block: 10px;
-		background-color: white;
 	}
 </style>
