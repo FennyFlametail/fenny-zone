d7c174e4d87f242320c645df2d91f86a150ab895
Fenny Flametail <fennyflametail@icloud.com>
2025-07-01 19:23:09 -0500

    feat: add Monty Hall problem

--- src/lib/apps.ts
+++ src/lib/apps.ts
@@ -1,4 +1,5 @@
 import * as toddspin from '../routes/toddspin/+page.svelte';
 import * as sauce from '../routes/sauce/+page.svelte';
+import * as goat from '../routes/goat/+page.svelte';
 
-export default [sauce, toddspin];
+export default [toddspin, sauce, goat];
---
+++ src/routes/goat/+page.svelte
@@ -0,0 +1,14 @@
+<script module lang="ts">
+	import Browser from '$lib/components/Browser.svelte';
+	import type { AppMetadata } from '$lib/types/AppTypes';
+	export const metadata: AppMetadata = {
+		key: 'goat',
+		title: 'Monty Hall Problem',
+		icon: 'icons/goat.png',
+		size: {
+			height: 800
+		}
+	};
+</script>
+
+<Browser src="https://monty-hall.fenny.zone" />
