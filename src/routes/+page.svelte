1e51383009545af12ca8ec3b6eaa0cb69b97cedb
Fenny Flametail <fennyflametail@icloud.com>
2025-07-01 19:23:07 -0500

    feat: save open apps to query string

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
+	import { replaceState } from '$app/navigation';
+	import { page } from '$app/state';
 	import Desktop from '$lib/components/Desktop.svelte';
 	import type AppInstance from '$lib/types/AppInstance';
 	import type AppMetadata from '$lib/types/AppMetadata';
-	import type { Component } from 'svelte';
+	import { untrack, type Component } from 'svelte';
 
 	import * as App1 from './app-1/+page.svelte';
 	import * as App2 from './app-2/+page.svelte';
+	const allApps = [App1, App2];
 
 	let apps: AppInstance[] = $state([]);
 
@@ -20,6 +23,24 @@
 	function closeApp(app: AppInstance) {
 		apps.splice(apps.indexOf(app), 1);
 	}
+
+	let initComplete = $state(false);
+	$effect(() => {
+		const url = new URL(untrack(() => page.url));
+		const paramsApps = url.searchParams.get('apps');
+		if (!initComplete && paramsApps && !apps.length) {
+			// open apps from query string on page load
+			const appsToOpen = paramsApps
+				.split(',')
+				.map((name) => allApps.find((app) => app.name === name));
+			appsToOpen.forEach((app) => app && openApp(app));
+		} else {
+			// update query string when apps are opened and closed
+			url.searchParams.set('apps', apps.map((app) => app.metadata.name).join(','));
+			replaceState(url, {});
+		}
+		initComplete = true;
+	});
 
 	function resetApps() {
 		apps.forEach((app) => app.window?.resetPosition());
