62ac1d2ea0952cb49329f46a122b120325629a4d
Fenny Flametail <fennyflametail@icloud.com>
2025-07-01 19:23:08 -0500

    feat: logo in menubar

--- src/lib/components/MenuBar.svelte
+++ src/lib/components/MenuBar.svelte
@@ -1,5 +1,18 @@
 <header class="menubar">
 	<details class="menuWrapper" name="menubar">
+		<summary class="menuName">
+			<span class="menuLogo">🦊</span>
+		</summary>
+		<menu class="menu">
+			<li class="menuItem">
+				<button>Menu Item Button</button>
+			</li>
+			<li class="menuItem">
+				<a class="menuItem" href="/">Menu Item Link</a>
+			</li>
+		</menu>
+	</details>
+	<details class="menuWrapper" name="menubar">
 		<summary class="menuName">Menu 1</summary>
 		<menu class="menu">
 			<li class="menuItem">
