f5811f2ce0acdd5bedb10c146992f6b6de39f16f
Fenny Flametail <fennyflametail@icloud.com>
2025-07-01 19:23:07 -0500

    feat: custom font

--- src/routes/app-1/+page.svelte
+++ src/routes/app-1/+page.svelte
@@ -4,9 +4,11 @@
 </script>
 
 <p>
-	Reprehenderit qui ipsum irure ut. Sit mollit aute ipsum officia incididunt occaecat Lorem labore
-	deserunt culpa proident enim mollit. Nulla nulla magna culpa sint enim labore nisi magna velit
-	duis consequat cillum. Id excepteur aliquip ea proident sit. Irure enim nostrud nostrud eu non do
-	officia nulla in enim laborum nostrud. Ullamco ea in adipisicing ea. Voluptate tempor proident
-	amet nulla sunt dolore anim elit.
+	<b>
+		Reprehenderit qui ipsum irure ut. Sit mollit aute ipsum officia incididunt occaecat Lorem labore
+		deserunt culpa proident enim mollit. Nulla nulla magna culpa sint enim labore nisi magna velit
+		duis consequat cillum. Id excepteur aliquip ea proident sit. Irure enim nostrud nostrud eu non
+		do officia nulla in enim laborum nostrud. Ullamco ea in adipisicing ea. Voluptate tempor
+		proident amet nulla sunt dolore anim elit.
+	</b>
 </p>
