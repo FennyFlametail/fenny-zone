1671a132fc5a4005baec6b64aa83b7cbe5bdf797
Fenny Flametail <fennyflametail@icloud.com>
2025-07-05 20:46:56 -0400

    fix: update readme

--- src/routes/readme/+page.svelte
+++ src/routes/readme/+page.svelte
@@ -3,7 +3,7 @@
 	let text = $state(
 		`Hi, I'm Fenny! Welcome to my dumb little corner of the internet 🦊❤️
 
-This site is a work-in-progress, but feel free to poke around!
+This site is a work-in-progress, but feel free to poke around! You can find me at the links on the right.
 
 TO-DO:
 - more content
