a484cb51509b12b6933fa6c0fe018cb3e8aff499
Fenny Flametail <fennyflametail@icloud.com>
2025-09-21 17:51:58 -0500

    fix: better typing for apps

--- src/lib/apps.svelte.ts
+++ src/lib/apps.svelte.ts
@@ -1,63 +1,45 @@
-import type { Component } from 'svelte';
-import Window, { type Position } from '$lib/components/Window.svelte';
 import Browser from '$lib/components/apps/Browser.svelte';
 import Characters from '$lib/components/pages/Characters.svelte';
-import Fenny from '$lib/components/pages/characters/Fenny.svelte';
 import Aren from '$lib/components/pages/characters/Aren.svelte';
 import Ceph from '$lib/components/pages/characters/Ceph.svelte';
+import Fenny from '$lib/components/pages/characters/Fenny.svelte';
 import Nocturne from '$lib/components/pages/characters/Nocturne.svelte';
 import Projects from '$lib/components/pages/Projects.svelte';
 import Readme from '$lib/components/pages/Readme.svelte';
+import Window from '$lib/components/Window.svelte';
+import type { Position } from '$lib/windowServer.svelte';
+import type { Component } from 'svelte';
 
-export interface AppEntry {
-	/** Page doesn't matter for parent apps */
-	Page: Component<any>;
-	/** Parent apps can't be launched directlly */
-	isParent?: boolean;
-	/** Apps will be grouped by their parent icon in the Dock */
-	parent?: keyof typeof apps;
-	title: string;
-	icon: string;
-	/** Route doesn't matter for parent apps */
-	route: string;
-	/** Used for the Browser component */
-	url?: string;
-	defaultSize?: {
-		/** @default 500 */
-		width?: number;
-		/** @default 500 */
-		height?: number;
-	};
-	instance?: {
-		window?: Window;
-		position: Position;
-		modified?: boolean;
-	};
-}
-export type RunningApp = AppEntry & { instance: Required<AppEntry>['instance'] };
+export type AppName =
+	| 'Finder'
+	| 'TextEdit'
+	| 'Trash'
+	| 'readme'
+	| 'characters'
+	| 'fenny'
+	| 'aren'
+	| 'ceph'
+	| 'nocturne'
+	| 'projects'
+	| 'toddspin'
+	| 'sauce'
+	| 'goat';
 
-const apps = $state({
+const apps = $state<Record<AppName, AppEntry>>({
 	Finder: {
-		// FIXME remove unnecessary Page from parents
-		Page: Readme,
 		isParent: true,
 		title: 'Finder',
-		icon: '/icons/finder.png',
-		route: ''
+		icon: '/icons/finder.png'
 	},
 	TextEdit: {
-		Page: Readme,
 		isParent: true,
 		title: 'TextEdit',
-		icon: '/icons/textedit.png',
-		route: ''
+		icon: '/icons/textedit.png'
 	},
 	Trash: {
-		Page: Readme,
 		isParent: true,
 		title: 'Trash',
-		icon: '/icons/trash.png',
-		route: ''
+		icon: '/icons/trash.png'
 	},
 	readme: {
 		parent: 'TextEdit',
@@ -135,6 +117,39 @@ const apps = $state({
 		url: 'https://monty-hall.fenny.zone'
 	}
 });
+export default apps;
 
-export type AppName = keyof typeof apps;
-export default apps as Record<AppName, AppEntry>;
+export type AppEntry = {
+	/** Parent apps can't be launched directly */
+	isParent?: boolean;
+	/** Apps will be grouped by their parent icon in the Dock */
+	parent?: AppName;
+	title: string;
+	icon: string;
+	/** Used for the Browser component */
+	url?: string;
+	defaultSize?: {
+		/** @default 500 */
+		width?: number;
+		/** @default 500 */
+		height?: number;
+	};
+	instance?: {
+		window?: Window;
+		position: Position;
+		modified?: boolean;
+	};
+} & (
+	| {
+			isParent: true;
+			Page?: never;
+			route?: never;
+	  }
+	| {
+			isParent?: false;
+			Page: Component<any>;
+			route: string;
+	  }
+);
+
+export type RunningApp = AppEntry & { instance: Required<AppEntry>['instance'] };
