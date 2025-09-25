176628fe85b8e17660317901dec764b588ccb461
Fenny Flametail <fennyflametail@icloud.com>
2025-09-24 20:08:01 -0500

    feat: use page state instead of load function to get path

--- src/routes/[...path]/+page.svelte
+++ src/routes/[...path]/+page.svelte
@@ -1,19 +1,17 @@
 <script lang="ts">
+	import { page } from '$app/state';
 	import type { AppName } from '$lib/apps.svelte';
 	import Desktop from '$lib/components/Desktop.svelte';
 	import Dock from '$lib/components/Dock.svelte';
 	import MenuBar from '$lib/components/MenuBar.svelte';
 	import WindowLayer from '$lib/components/WindowLayer.svelte';
 	import { apps } from '$lib/windowServer.svelte';
-	import type { PageProps } from './$types';
-
-	const { data }: PageProps = $props();
 
 	let initialApp: AppName | undefined = $state();
-	if (data.params.path) {
+	if (page.params.path) {
 		initialApp = (Object.keys(apps) as AppName[]).find((appName) => {
 			const app = apps[appName];
-			return app.route === '/'.concat(data.params.path!);
+			return app.route === '/'.concat(page.params.path);
 		});
 	}
 </script>
--- src/routes/[...path]/+page.ts
+++
@@ -1,5 +0,0 @@
-import type { LoadEvent } from '@sveltejs/kit';
-
-export function load({ url, route, params }: LoadEvent) {
-	return { url, route, params };
-}
