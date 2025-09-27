d529a933fdcc445dd82d9f01c7396949db4a9e16
Fenny Flametail <fennyflametail@icloud.com>
2025-09-27 15:08:06 -0500

    feat: update readme

--- src/lib/components/pages/Readme.svelte
+++ src/lib/components/pages/Readme.svelte
@@ -3,13 +3,14 @@
 	let text = $state(
 		`Hi, I'm Fenny! Welcome to my dumb little corner of the internet 🦊❤️
 
-This site is a work-in-progress, but feel free to poke around! You can find me at the links on the right.
+This site is a work-in-progress, but feel free to poke around! You can find me elsewhere on the web at the links on the right.
+
+(Hint: the folders at the bottom of the screen are clickable. To open icons on the desktop or in folders, double-click.)
 
 TO-DO:
 - more content
 - better mobile layout
 - themes
-- more functionality without JavaScript
 
 Googly eyes inspired by Sindre Sorhus: https://sindresorhus.com/googly-eyes`
 	);
