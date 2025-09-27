5fb6de85e96122f5048c972f4e85b7dec02a9c14
Fenny Flametail <fennyflametail@icloud.com>
2025-09-27 17:16:47 -0500

    fix: add poster image to Trash app

--- src/lib/components/Trash.svelte
+++ src/lib/components/Trash.svelte
@@ -1,11 +1,25 @@
-<video src="/trash.mp4" muted autoplay loop playsinline disablepictureinpicture
-	>Baby fox jumping into a trash can</video
->
+<div class="videoContainer">
+	<video
+		src="/trash.mp4"
+		poster="/trash-poster.webp"
+		muted
+		autoplay
+		loop
+		playsinline
+		disablepictureinpicture>Baby fox jumping into a trash can</video
+	>
+</div>
 
 <style>
-	video {
-		height: 100%;
+	.videoContainer {
 		width: 100%;
+		height: 100%;
+		background-color: black;
+	}
+
+	video {
+		width: 100%;
+		height: 100%;
 		object-fit: cover;
 	}
 </style>
