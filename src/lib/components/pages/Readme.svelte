e2fe5caaf7ca4eaa0c38af7370e959481d294316
Fenny Flametail <fennyflametail@icloud.com>
2026-02-08 17:22:29 -0600

    feat: update readme

--- src/lib/components/pages/Readme.svelte
+++ src/lib/components/pages/Readme.svelte
@@ -2,6 +2,8 @@
 	import { getAppContext } from '$lib/context.svelte';
 	import setModified from '$lib/helpers/setModifiedAttachment.svelte';
 	import FennyWave from '$lib/images/fenny-wave.webp';
+	import NoAi from '$lib/images/no_ai.png';
+	import MwMac from '$lib/images/mwmac.png';
 
 	const { app } = getAppContext();
 </script>
@@ -9,8 +11,12 @@
 <pre class="textEdit plaintext" contenteditable="true" {@attach setModified(app)}>
 Hi, I'm Fenny! Welcome to my silly corner of the internet 🦊❤️
 
-<img src={FennyWave} alt="My fox Fenny, waving at you!" width="256px" height="256px" />
+<img src={FennyWave} alt="My fox Fenny, waving at you!" width="256" height="256" />
 This site is a work-in-progress, but feel free to poke around! Check out the icons at the bottom, or find me elsewhere at the links on the right!
+
+(Fun fact: almost everything on this site works with JavaScript disabled!)
+
+Scroll down for credits ⬇️
 
 Waving sticker by <a
 		href="https://www.furaffinity.net/user/ponacho"
@@ -31,5 +37,22 @@ Chozo font by <a
 		href="https://old.reddit.com/r/Metroid/comments/qm2v3c/i_have_made_a_fully_functional_automated/"
 		target="_blank"
 		contenteditable="false">Wolfeur</a
+	> (no longer available)
+No AI badge by <a href="https://samvieten.itch.io/no-ai" target="_blank" contenteditable="false"
+		>Sam Vieten</a
 	>
+
+<div class="badges">
+	<img src={NoAi} alt="NO AI - Handmade" />
+	<img src={MwMac} alt="Made on a Mac" />
+</div>
 </pre>
+
+<style>
+	.badges {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 20px;
+	}
+</style>
