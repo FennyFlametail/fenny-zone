04364d0604c8a439b6b98d3636f7d7a18ba654da
Fenny Flametail <fennyflametail@icloud.com>
2026-03-11 21:49:34 -0500

    feat: update Bluesky play button style

--- src/routes/bluesky/+page.svelte
+++ src/routes/bluesky/+page.svelte
@@ -733,8 +733,15 @@
 			left: 50%;
 			top: 50%;
 			translate: -50% -50%;
-			font-size: 72px;
-			color: rgb(255 255 255 / 50%);
+			width: 72px;
+			height: 72px;
+			display: grid;
+			place-items: center;
+			padding-left: 7.5px;
+			background-color: rgb(255 255 255 / 75%);
+			border-radius: 50%;
+			font-size: 42px;
+			color: rgb(0 0 0 / 75%);
 		}
 
 		.blueskyLinkPreview {
