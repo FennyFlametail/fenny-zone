963b40394a77e22dd88ac6baa448ea1a036747dd
Fenny Flametail <fennyflametail@icloud.com>
2026-04-17 22:01:53 -0500

    feat: try to move focus into window on open

--- src/lib/components/Window.svelte
+++ src/lib/components/Window.svelte
@@ -6,6 +6,7 @@
 	import WindowControls from '$lib/components/WindowControls.svelte';
 	import { getWindowServerContext, setAppContext } from '$lib/context.svelte';
 	import WindowServer from '$lib/windowServer.svelte';
+	import { onMount } from 'svelte';
 	import { prefersReducedMotion } from 'svelte/motion';
 	import { scale } from 'svelte/transition';
 
@@ -31,6 +32,15 @@
 	}
 
 	let element = $state<HTMLElement>();
+	let contentWrapper = $state<HTMLDivElement>();
+
+	onMount(() => {
+		// focus the first focusable element in the window
+		const focusableElement = contentWrapper?.querySelector(
+			'a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, [tabindex], [contentEditable=true]'
+		);
+		if (focusableElement) (focusableElement as HTMLElement).focus();
+	});
 
 	let dragging = $state(false);
 	let resizing = $state(false);
@@ -181,7 +191,7 @@
 			{/if}
 		</header>
 	{/if}
-	<div class="windowContent">
+	<div bind:this={contentWrapper} class="windowContent">
 		{#if browser}
 			<svelte:boundary onerror={(e) => windowServer.closeAppWithError(appName, e)}>
 				<app.Page {...props} />
