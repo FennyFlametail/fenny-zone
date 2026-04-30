80a1bc4a2fe55bbfcbe5ec5891a67a10305b6fa7
Fenny Flametail <fennyflametail@icloud.com>
2026-04-29 21:11:55 -0500

    feat: change default desktop picture

--- src/lib/helpers/getDesktopPicture.svelte.ts
+++ src/lib/helpers/getDesktopPicture.svelte.ts
@@ -10,7 +10,7 @@ export async function getDesktopPicture(windowServer: WindowServer): Promise<Des
 		pic = desktopPictures[windowServer.preferences.desktopPicture];
 	}
 
-	if (!pic) pic = desktopPictures.beach;
+	if (!pic) pic = desktopPictures.breakfast;
 
 	return pic;
 }
