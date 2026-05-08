21fdc597127b7b022bffcd716344d8a0e03e3bf3
Fenny Flametail <fennyflametail@icloud.com>
2026-05-07 23:43:25 -0500

    fix: undo user-select on windowLayer (breaks text selection)

--- src/lib/components/WindowLayer.svelte
+++ src/lib/components/WindowLayer.svelte
@@ -27,8 +27,6 @@
 		position: relative;
 		display: grid;
 		pointer-events: none; /* allow clicks to fall through to the desktop */
-		-webkit-user-select: none;
-		user-select: none;
 
 		> :global(*) {
 			pointer-events: auto;
