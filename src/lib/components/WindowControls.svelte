7ffb8710fc90e2442b42dc5139fb3359107add47
Fenny Flametail <fennyflametail@icloud.com>
2026-09-19 23:11:40 -0500

    feat: improve window button styling

--- src/lib/components/WindowControls.svelte
+++ src/lib/components/WindowControls.svelte
@@ -63,7 +63,7 @@
 	}
 
 	.windowButton {
-		--base-box-shadow: inset 0 0 1px 1px rgb(0 0 0 / 50%), var(--widget-sunken-box-shadow);
+		--base-box-shadow: inset 0 1px 2px 0 rgb(0 0 0 / 75%), var(--widget-sunken-box-shadow);
 		width: 18px;
 		height: 18px;
 		display: flex;
